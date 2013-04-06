97e45f660662ceed34801369a1e740d3ee736249
Toby Cox <toby@carnivallabs.com>
2013-04-06 17:46:49 +0100

    Adding a radar search method

--- lib/google_places/request.rb
+++ lib/google_places/request.rb
@@ -12,6 +12,7 @@ module GooglePlaces
     DETAILS_URL       = 'https://maps.googleapis.com/maps/api/place/details/json'
     TEXT_SEARCH_URL   = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
     PAGETOKEN_URL     = 'https://maps.googleapis.com/maps/api/place/search/json'
+    RADAR_SEARCH_URL         = 'https://maps.googleapis.com/maps/api/place/radarsearch/json'
 
     # Search for Spots at the provided location
     #
@@ -55,6 +56,51 @@ module GooglePlaces
     # @see https://developers.google.com/maps/documentation/places/supported_types List of supported types
     def self.spots(options = {})
       request = new(NEARBY_SEARCH_URL, options)
+      request.parsed_response
+    end
+
+    # Search for Spots at the provided location, using the RadarSearch API. Up to 200 results may be returned
+    #
+    # @return [Array<Spot>]
+    # @param [Hash] options
+    # @option options [String] :key
+    #   the provided api key.
+    #   <b>Note that this is a mandatory parameter</b>
+    # @option options [Integer] :radius
+    #   Defines the distance (in meters) within which to return Place results.
+    #   The maximum allowed radius is 50,000 meters.
+    #   Note that radius must not be included if <b>:rankby</b> is specified
+    #   <b>Note that this is a mandatory parameter</b>
+    # @option options [Boolean] :sensor
+    #   Indicates whether or not the Place request came from a device using a location sensor (e.g. a GPS) to determine the location sent in this request.
+    #   <b>Note that this is a mandatory parameter</b>
+    # @option options [(Integer,Integer),String] :location
+    #   The latitude/longitude around which to retrieve Spot information. This must be specified as latitude,longitude
+    #   <b>Note that this is a mandatory parameter</b>
+    # @option options [String,Array] :types
+    #   Restricts the results to Spots matching at least one of the specified types
+    # @option options [String] :name
+    #   A term to be matched against the names of Places.
+    #   Results will be restricted to those containing the passed name value.
+    # @option options [String] :keyword
+    #   A term to be matched against all content that Google has indexed for this Spot,
+    #   including but not limited to name, type, and address,
+    #   as well as customer reviews and other third-party content.
+    # @option options [String] :language
+    #   The language code, indicating in which language the results should be returned, if possible.
+    # @option options [String,Array<String>] :exclude ([])
+    #   A String or an Array of <b>types</b> to exclude from results
+    #
+    # @option options [Hash] :retry_options ({})
+    #   A Hash containing parameters for search retries
+    # @option options [Object] :retry_options[:status] ([])
+    # @option options [Integer] :retry_options[:max] (0) the maximum retries
+    # @option options [Integer] :retry_options[:delay] (5) the delay between each retry in seconds
+    #
+    # @see http://spreadsheets.google.com/pub?key=p9pdwsai2hDMsLkXsoM05KQ&gid=1 List of supported languages
+    # @see https://developers.google.com/maps/documentation/places/supported_types List of supported types
+    def self.spots_by_radar(options = {})
+      request = new(RADAR_SEARCH_URL, options)
       request.parsed_response
     end
 
